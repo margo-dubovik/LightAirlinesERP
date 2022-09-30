be3dae170ba38fcba7c6b5864bed902447ec4be3
margo-dubovik <margodubovik531@gmail.com>
2022-09-30 15:46:00 +0300

    removed odd lines

--- airline_erp/passenger_interface/urls.py
+++ airline_erp/passenger_interface/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('profile/bookings/upcoming', views.profile_upcoming_bookings, name='upcoming-bookings'),
     path('profile/bookings/upcoming/<int:booking_id>', views.booking_details_view, name='booking-details'),
     path('profile/bookings/previous', views.profile_previous_bookings, name='previous-bookings'),
-    path('profile/checkin', views.profile_online_check_in, name='online-checkin'),
-    # path('airport_autocomplete', views.AirportAutocomplete.as_view(), name='airport-autocomplete',),
 ]
